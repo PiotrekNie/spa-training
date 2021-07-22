6d62cd48bbb4bba093645bd1f7085863f78096d0
PiotrekNiemirski <piotr.niemirski@they.pl>
2021-07-22 22:13:38 +0200

    @nestjs - components

--- nestjs-cars-app/src/components/cars/cars.resolver.ts
+++ nestjs-cars-app/src/components/cars/cars.resolver.ts
@@ -1,12 +1,15 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Car } from './entities/car';
 import { CarsService } from './cars.service';
 
 @Resolver()
 export class CarsResolver {
   constructor(private carsService: CarsService) {}
 
-  @Query((returns) => String)
-  public async cars() {
-    return 'Hello and welcome to Yourcar shop!';
+  @Query((returns) => [Car])
+  public async cars(): Promise<Car[]> {
+    return await this.carsService.getAllCars().catch((err) => {
+      throw err;
+    });
   }
 }
